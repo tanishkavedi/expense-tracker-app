9deb3ee868bb21cd89c851a0f547c99d675e62a4
tanishkavedi <160759271+tanishkavedi@users.noreply.github.com>
2026-06-19 13:08:16 +0530

    add database operations for user management

---
+++ backend/src/models/userModel.ts
@@ -0,0 +1,26 @@
+import pool from "../config/db";
+import { User } from "../types";
+
+export const createUser = async (
+  name : string,
+  email: string,
+  hashedPassword: string
+): Promise<User> => {
+  const result = await pool.query(
+    `INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3) RETURNING *`,
+    [name, email, hashedPassword]
+    
+  );
+  return result.rows[0];
+};
+
+export const findUserByEmail = async (email: string): Promise<User | null> => {
+  const result = await pool.query(
+    `SELECT * FROM users WHERE email = $1`,
+    [email]
+  );
+
+  return result.rows[0] || null;
+
+};
