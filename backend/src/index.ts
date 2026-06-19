485565fcaea0852a597ae8d9849dab5133605432
tanishkavedi <160759271+tanishkavedi@users.noreply.github.com>
2026-06-19 14:42:29 +0530

    update the features

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,30 +3,6 @@ import cors from "cors";
 import dotenv from "dotenv";
 import "./config/db";
 import authRoutes from "./routes/authRoutes";
-
-dotenv.config();
-
-const app: Application = express();
-const PORT = process.env.PORT || 5001;
-
-app.use(cors());
-app.use(express.json());
-
-
-app.use("/api/auth", authRoutes);
-
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Expense Tracker API running");
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});import express, { Application, Request, Response } from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import "./config/db";
-import authRoutes from "./routes/authRoutes";
 import categoryRoutes from "./routes/categoryRoutes";
 import transactionRoutes from "./routes/transactionRoutes";
 
